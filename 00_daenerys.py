3acfa2f9cefa69d7a677761f3f8bd0e2ecbf263a
Kavish Kartha <38027212+kavishkartha1o1@users.noreply.github.com>
2019-11-03 17:24:07 -0800

    Add files via upload

---
+++ 00_daenerys.py
@@ -0,0 +1,7 @@
+userInput = input("Who goes there?")
+if userInput == "Daenerys of the House Targaryen, the First of Her Name, The Unburnt, Queen of the Andals, the Rhoynar and the First Men, Queen of Meereen, Khaleesi of the Great Grass Sea, Protector of the Realm, Lady Regnant of the Seven Kingdoms, Breaker of Chains and Mother of Dragons" or "DHTFHNUQARFMQMKGSPRLRSKBCMD":
+    print("Welcome, Daenerys")
+elif userInput == "Dany":
+    print("Dany who?")
+else:
+    print("Move along, now.")
